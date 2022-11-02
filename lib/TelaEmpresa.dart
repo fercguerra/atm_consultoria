68c2aa78c0c163f531b2cbbad678863ada7a0b64
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-11-02 19:31:17 -0300

    Empresa e Serviço

--- lib/TelaEmpresa.dart
+++ lib/TelaEmpresa.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TelaEmpresa extends StatefulWidget {
-  const TelaEmpresa({ Key? key }) : super(key: key);
+  const TelaEmpresa({Key? key}) : super(key: key);
 
   @override
   State<TelaEmpresa> createState() => _TelaEmpresaState();
@@ -10,8 +10,38 @@ class TelaEmpresa extends StatefulWidget {
 class _TelaEmpresaState extends State<TelaEmpresa> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Empresa"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset('images/detalhe_empresa.png'),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Sobre a Empresa",
+                      style: TextStyle(fontSize: 20, color: Colors.deepOrange),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: const Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/TelaServico.dart
+++ lib/TelaServico.dart
@@ -10,8 +10,40 @@ class TelaServico extends StatefulWidget {
 class _TelaServicoState extends State<TelaServico> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return
+      Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Serviços"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset('images/detalhe_servico.png'),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Sobre a Empresa",
+                      style: TextStyle(fontSize: 20, color: Colors.deepOrange),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: const Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
